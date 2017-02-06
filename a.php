f4d12797b723c6d74538407bc3cd21acde0fed44
Gary Lamperillo <garylamperillo@Garys-MacBook-Pro.local>
2017-02-06 10:17:47 -0800

    a.php

---
+++ a.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+
+<html>
+<head>
+  <title>Canary A - Welcome to your CakePHP application on OpenShift</title>
+</head>
+
+<body>
+<b>Welcome to your CakePHP application on OpenShift</b>
+
+
+
+<b> Canary A </b>
+</body>
+
+
+</html>
+
+
+
+echo "OK";
+?>
